7c7c35a73efa4ccab75cec7e7a5fca3963b67e56
hallya <lucien.dulac@gmail.com>
2019-04-22 23:58:22 +0200

    content added in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,37 @@
 <template>
-  <main></main>
+  <main>
+    <section>
+      <h2>Retrouver l’Energie, Soigner par le Shiatsu</h2>
+      <blockquote>
+        « …le soin, lorsqu’il se conçoit comme relation et attention à la souffrance et aux besoins du malade, peut réparer son image dévastée, réveiller son désir de vivre, le libérer de l’amertume ou l’inquiétude propres à la maladie. »
+      </blockquote>
+      <cite>
+        Claire Marin – La maladie, catastrophe intime
+      </cite>
+    </section>
+    <section>
+      <p>Fatigué ou vite fatigable, anxieux, déprimé, tendu, irascible, colérique, à fleur de peau, agité, angoissé, stressé, triste, sans entrain, sans énergie, sans appétit, en train de suivre un régime, de santé fragile, toujours enrhumé, sujet aux gastro à répétition, aux maux de dos, aux tensions dans les épaules et la nuque, aux douleurs articulaires, aux points douloureux se déplaçant ici ou là, aux crampes et/ou aux jambes lourdes, en burn out, épuisée par une grossesse, par l’accouchement, par l’allaitement, en baby blues, en période de ménopause, douloureux à la suite d’un accident, d’une fracture, d’une opération, en période d’études difficiles, dans le trac d’un examen, en traitement pour le cancer, dans le chagrin à la suite d’une séparation, d’un deuil, dans l’angoisse d’une maladie incurable, en fin de vie…</p>
+      <p>Ou simplement pour prévenir plutôt que guérir, pour avoir plus de force, plus de calme, plus d’entrain et de légèreté, pour se sentir un peu ou beaucoup mieux…</p>
+      <p>Le shiatsu vous apporte le soin qui vous permet selon les cas de vous rétablir, d’être réaccordé, un ancrage et un apaisement dans la situation qui est la vôtre.</p>
+      <p>J’ai constitué cette liste à partir des maux, légers ou graves, passagers ou persistants pour lesquels femmes, hommes ou enfants sont venus me voir et à qui j’ai fait du bien en réunifiant l’être, corps, cœur, esprit.</p>
+    </section>
+  </main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-@Component({
-})
+@Component({})
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss">
+section {
+  h2 {
+    text-align: left;
+  }
+  blockquote {
+    margin: 0 10px;
+  }
+} 
+</style>
