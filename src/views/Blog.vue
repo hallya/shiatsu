0fe188c30cbd36e999f3b9092032b348a27867a1
hallya <lucien.dulac@gmail.com>
2019-05-04 11:31:04 +0100

    view exported as module

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -3,3 +3,10 @@
     <h1>Blog</h1>
   </main>
 </template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class Blog extends Vue {}
+</script>
