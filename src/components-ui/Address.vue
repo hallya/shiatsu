3df79e54783646a5a60063b1af0f26411890782f
hallya <lucien.dulac@gmail.com>
2019-08-28 22:50:43 +0200

    refacto of contact (create Address component-ui

---
+++ src/components-ui/Address.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="address">
+    <figure>
+      <picture>
+        <img :src="`https://maps.googleapis.com/maps/api/staticmap?center=${mapsApiAddressName}&zoom=17&size=800x400&maptype=roadmap&key=AIzaSyBIpZM5Rd2uy9aYV3_DByJrYS1okNf3dJY`" alt="Plan d'accès au cabinet de Saint-Cyr-l'Ecole">
+      </picture>
+      <figcaption>
+        <p v-for="line of fullAddress" v-bind:key="line">{{line}}</p>
+      </figcaption>
+    </figure>
+  </section>
+</template>
+<script>
+export default {
+  name: 'Address',
+  props: ['mapsApiAddressName', 'fullAddress'],
+};
+</script>
+<style lang="scss">
+@import '@/main.scss';
+
+section.address {
+  border: 1px solid $light-gray;
+  border-radius: 8px;
+  padding: 0 0 10px;
+
+  @include laptop {
+    width: 45%;
+  }
+  img {
+    width: 100%;
+  }
+  p {
+    padding: 0 20px;
+  }
+}
+</style>
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,32 +1,48 @@
 <template>
-  <main>
+  <main id="contact">
     <address>
       <h2>Coordonnées</h2>
-      <section>
+      <section class="contact">
         <h3>Nathalie de Loeper</h3>
         <a href="tel:+33612387928">06 12 38 79 28</a>
+        <span>-</span>
         <a href="mailto:natloeper@gmail.com">natloeper@gmail.com</a>
       </section>
       <h2>Adresses</h2>
-      <section class="address">
-        <img src="https://maps.googleapis.com/maps/api/staticmap?center=deloepershiatsu,saint-cyr-lecole&zoom=17&size=800x400&maptype=roadmap&key=AIzaSyBIpZM5Rd2uy9aYV3_DByJrYS1okNf3dJY" alt="Plan d'accès au cabinet de Saint-Cyr-l'Ecole">
-        <p>5 rue Jean Zay</p>
-        <p>78210 Saint-Cyr-l'Ecole</p>
-      </section>
-      <section class="address">
-        <img src="https://maps.googleapis.com/maps/api/staticmap?center=nathaliedeloeper,bouloire&zoom=17&size=800x400&maptype=roadmap&key=AIzaSyBIpZM5Rd2uy9aYV3_DByJrYS1okNf3dJY" alt="Plan d'accès au cabinet de Saint-Cyr-l'Ecole">
-        <p>La Grenouille</p>
-        <p>Route de Coudrecieux</p>
-        <p>72440 Bouloire</p>
-      </section>
+      <Address :mapsApiAddressName="saintCyrLecole.mapApiName" :fullAddress="saintCyrLecole.fullAddress"/>
+      <Address :mapsApiAddressName="bouloire.mapApiName" :fullAddress="bouloire.fullAddress"/>
     </address> 
   </main>
 </template>
 
+<script>
+import Address from '@/components-ui/Address';
+
+export default {
+  components: {
+    Address
+  },
+  data() {
+    return {
+      bouloire: {
+        mapApiName: 'nathaliedeloeper,bouloire',
+        fullAddress: ['La Grenouille', 'Route de Coudrecieux', '72440 Bouloire'],
+      },
+      saintCyrLecole: {
+        mapApiName: 'deloepershiatsu,saint-cyr-lecole',
+        fullAddress: ['5 rue Jean Zay', '78210 Saint-Cyr-l\'Ecole'],
+      },
+    }
+  }
+}
+</script>
+
 <style lang="scss">
 @import '@/main.scss';
-
-address {
+main#contact {
+  display: flex;
+  
+  address {
     margin: auto;
     text-align: justify;
     
@@ -49,17 +65,11 @@ address {
       margin: 10px 0;
       padding: 10px 20px;
 
-      &.address {
-        border: 1px solid $light-gray;
-        border-radius: 8px;
-        padding: 0 0 10px;
-
+      &.contact {
         @include laptop {
-          width: 45%;
-        }
-
-        p {
-          padding: 0 20px;
+          display: flex;
+          justify-content: center;
+          flex-flow: row wrap;
         }
       }
       h3, a, p {
@@ -78,15 +88,25 @@ address {
       h3 {
         margin-bottom: 15px;
         text-decoration: underline;
+        
+        @include tablet {
+          width: 100%;
+          text-align: center;
+        }
       }
       a {
         color: $jungle-green;
+        
+        @include tablet {
+          display: inline;
+        }
       }
-      a:not(:last-child) {
-        margin-bottom: 10px;
-      }
-      img {
-        width: 100%;
+      span {
+        display: none;
+        @include tablet {
+          display: initial;
+          margin: 0 10px;
+        }
       }
       p {
         line-height: 28px;
@@ -94,4 +114,5 @@ address {
       }
     }
   }
+}
 </style>
