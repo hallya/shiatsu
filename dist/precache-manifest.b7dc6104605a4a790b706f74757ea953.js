a050b64392dd73d3a0e74464c2fb7d636bea05c2
hallya <lucien.dulac@gmail.com>
2019-09-04 22:41:43 +0200

    build pushed to gh-pages on Mer  4 sep 2019 22:41:43 CEST

---
+++ dist/precache-manifest.b7dc6104605a4a790b706f74757ea953.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "32dd225437a580a449f309c1cd455524",
+    "url": "/shiatsu/img/defaultImageForSharing.32dd2254.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shiatsu/robots.txt"
+  },
+  {
+    "revision": "ee0c1e8a7ac0bbf2c8a4f8295f9b522e",
+    "url": "/shiatsu/img/phone-filled.ee0c1e8a.svg"
+  },
+  {
+    "revision": "2bff97755d13fd4926f4",
+    "url": "/shiatsu/js/app.552290f7.js"
+  },
+  {
+    "revision": "37911ec0e34cd5de7948",
+    "url": "/shiatsu/js/chunk-vendors.60238380.js"
+  },
+  {
+    "revision": "4bfac5bcf3355b3a4750",
+    "url": "/shiatsu/js/contacts.7166ebb1.js"
+  },
+  {
+    "revision": "a6783e102431ed79e7e9",
+    "url": "/shiatsu/js/reviews.1fca3745.js"
+  },
+  {
+    "revision": "c8642c161897d62a44a6c4f0a7cd498a",
+    "url": "/shiatsu/img/nenuphars_et_fleurs.c8642c16.jpg"
+  },
+  {
+    "revision": "2341c50e6fe0ec1f19d0",
+    "url": "/shiatsu/js/about.4dbc959e.js"
+  },
+  {
+    "revision": "604f4f1577aa0464274a637352454f11",
+    "url": "/shiatsu/img/blog.604f4f15.svg"
+  },
+  {
+    "revision": "ac34f08256b6325a5f7306af429b06e1",
+    "url": "/shiatsu/fonts/arbutus-slab.ac34f082.otf"
+  },
+  {
+    "revision": "bed4f3aa0279b9f635d30d37d14a283d",
+    "url": "/shiatsu/fonts/raleway.bed4f3aa.otf"
+  },
+  {
+    "revision": "32e8e5e0a6ce78cd2cd4235f4892deca",
+    "url": "/shiatsu/img/home.32e8e5e0.svg"
+  },
+  {
+    "revision": "cc66f89722275cce44974cc23a6273e5",
+    "url": "/shiatsu/img/social-media-f-full.cc66f897.svg"
+  },
+  {
+    "revision": "d992afc5a813a4e54e9fcdc0145b0659",
+    "url": "/shiatsu/index.html"
+  },
+  {
+    "revision": "782d9d8fa2990e8f582cc95c24f27bbf",
+    "url": "/shiatsu/img/about.782d9d8f.svg"
+  },
+  {
+    "revision": "4c236f7266c5977059f74376e61b8ed7",
+    "url": "/shiatsu/img/nenuphar.4c236f72.svg"
+  },
+  {
+    "revision": "cf0588d68382db25ff51cce2dfcbe658",
+    "url": "/shiatsu/img/plateau_nenuphar.cf0588d6.jpg"
+  },
+  {
+    "revision": "e6582e3189d8caf3c74015170796d475",
+    "url": "/shiatsu/fonts/arbutus-slab.e6582e31.woff2"
+  },
+  {
+    "revision": "6301a29dd6884ef153c8629bbcf2ee11",
+    "url": "/shiatsu/fonts/raleway.6301a29d.woff2"
+  },
+  {
+    "revision": "5611c7afa60d1a7a494c337ce110cb65",
+    "url": "/shiatsu/img/phone-not-filled.5611c7af.svg"
+  },
+  {
+    "revision": "c9be947b3e1b6601a8089b3ad6abe07e",
+    "url": "/shiatsu/img/reviews.c9be947b.svg"
+  },
+  {
+    "revision": "b85e70d32c80448cebb1ad81e49893f0",
+    "url": "/shiatsu/img/contacts.b85e70d3.svg"
+  },
+  {
+    "revision": "49fd564264e5e62583f4abfbdcd5e9c8",
+    "url": "/shiatsu/img/tournesol.49fd5642.jpg"
+  },
+  {
+    "revision": "2ac46cc6784303a2a75c1171364ead7c",
+    "url": "/shiatsu/img/calendar.2ac46cc6.svg"
+  },
+  {
+    "revision": "7f0849c5f416b8343214d97ba51e3967",
+    "url": "/shiatsu/img/social-media-f-empty.7f0849c5.svg"
+  },
+  {
+    "revision": "a6783e102431ed79e7e9",
+    "url": "/shiatsu/css/reviews.1fb1cc14.css"
+  },
+  {
+    "revision": "4bfac5bcf3355b3a4750",
+    "url": "/shiatsu/css/contacts.b9650b07.css"
+  },
+  {
+    "revision": "2bff97755d13fd4926f4",
+    "url": "/shiatsu/css/app.4a880825.css"
+  }
+];
