1f97c1b248cf5d2df452c10aa55e920e396bfeb5
hallya <lucien.dulac@gmail.com>
2019-09-15 21:57:24 +0200

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
-git checkout gh-pages \
-&& git rebase master \
+git checkout deploy \
+&& git reset --hard master \
 && git push --force-with-lease \
+&& git push --force-with-lease origin :gh-pages \
 && npm run build \
 && git add dist/ \
 && git commit -m "build pushed to gh-pages on `date`" \
-&& git subtree push --prefix=dist origin gh-pages \
+&& git subtree push --prefix dist origin gh-pages \
 && git checkout master
