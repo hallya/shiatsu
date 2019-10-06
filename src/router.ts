bfc346dcc2af60ad3ee26d5e2815c9276bd78b62
hallya <lucien.dulac@gmail.com>
2019-10-06 12:59:52 +0200

    SEO: sitemap + history mode

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Home from '@/views/Home.vue';
 import ListPost from './components/ListPost.vue';
 import SinglePost from './components/SinglePost.vue';
 
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       name: 'home',
