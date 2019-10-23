812bd5eded3b8654e5726f21b0759e3b0af085dd
hallya <lucien.dulac@gmail.com>
2019-10-23 15:02:31 +0200

    Picture: alt attribute added even if undefined

--- src/components-ui/Picture/Picture.vue
+++ src/components-ui/Picture/Picture.vue
@@ -1,12 +1,17 @@
 <template>
-  <picture>
+  <picture v-if="loadImage">
     <source
+      v-if="image.imageWebp"
       media="(min-width: 500px)"
       :srcset="image.imageWebp"
       type="image/webp"
     />
-    <source :srcset="image.imageWebpMobile" type="image/webp" />
-    <img v-if="loadImage" :src="image.defaultImage" :alt="description" />
+    <source
+      v-if="image.imageWebpMobile"
+      :srcset="image.imageWebpMobile"
+      type="image/webp"
+    />
+    <img :src="image.defaultImage" :alt="description || ''" />
   </picture>
 </template>
 
