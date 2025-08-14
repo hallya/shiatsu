df5b23ae46eccb93e61ff2295864c65dcf7ee6c5
Lucien Dulac <coulterfeith@live.fr>
2025-08-14 17:35:24 +0200

    🧹 Remove obsolete COVID SpecialAnnouncement
    - Remove SpecialAnnouncement with expired date (2021-08-31)
    - Clean up homePage schema to remove outdated COVID information
    - Reduce bundle size by removing unnecessary structured data

--- src/data/googleSchemas/homePage.ts
+++ src/data/googleSchemas/homePage.ts
@@ -57,23 +57,5 @@ export const homePageSchemaContext: WithContext<MedicalBusiness | AboutPage> = {
         ],
       },
     },
-    {
-      "@type": "SpecialAnnouncement",
-      name: "Infos covid",
-      text:
-        "Pendant le confinement ou au-delà de l'horaire imposé par le couvre-feu vous pouvez venir recevoir un shiatsu. Il suffit de cocher la case de l'attestation : déplacement pour soins ne pouvant être assurés à distance, etc... Le shiatsu thérapeutique, médecine alternative, entre dans la liste des activités de santé humaine non classées ailleurs, sa pratique est autorisée. Les rendez-vous sont espacés de 2 heures afin d'aérer longuement après chaque patient. Il n'y a pas d'attente ni de croisement de personnes. Port du masque obligatoire.",
-      datePosted: "2021-01-20",
-      category: "https://www.wikidata.org/wiki/Q81068910",
-      announcementLocation: {
-        "@type": "MedicalBusiness",
-        "@id": "medicalBusiness",
-        address: {
-          "@id": "address",
-        },
-        name: "Cabinet sur Saint-Cyr-l'École",
-        url: organization["url"],
-      },
-      expires: "2021-08-31",
-    },
   ],
 };
