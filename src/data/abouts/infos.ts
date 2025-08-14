f599184ac111c5654852fc0cd3cab7b7c033a74b
Lucien Dulac <coulterfeith@live.fr>
2025-08-14 14:38:26 +0200

    feat: update price

--- src/data/abouts/infos.ts
+++ src/data/abouts/infos.ts
@@ -9,7 +9,7 @@ export const infos: FAQElement = {
   answer: [
     "Concernant la tarification,",
     "<strong>Saint Cyr l’Ecole</strong> 70 euros",
-    "<strong>Bouloire</strong> 55 euros",
+    "<strong>Bouloire</strong> 60 euros",
   ],
   images: {
     defaultImage: jpg,
